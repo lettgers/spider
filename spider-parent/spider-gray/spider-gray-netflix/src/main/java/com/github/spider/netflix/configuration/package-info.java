2b48484c8a993facbce87a1aee57860b54a74db3
曹鹏鹏 <caopengpeng@vsulv.com>
2021-06-04 18:49:40 +0800

    调整项目结构

---
+++ spider-parent/spider-gray/spider-gray-netflix/src/main/java/com/github/spider/netflix/configuration/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @Author: lettger
+ * @Date: 2021/6/4 6:48 下午
+ */
+package com.github.spider.netflix.configuration;
---
+++ spider-parent/spider-gray/spider-gray-netflix/src/main/java/com/github/spider/netflix/hystrix/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @Author: lettger
+ * @Date: 2021/6/4 6:47 下午
+ */
+package com.github.spider.netflix.hystrix;
